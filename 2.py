11449dc348f67d8cb94a5c58767fcbecdd808b6d
asyaryzhikhina <142721089+asyaryzhikhina@users.noreply.github.com>
2025-03-11 16:38:17 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,1 +1,14 @@
 #Дан массив целых чисел. Необходимо повернуть (сдвинуть) справа налево часть массива, которая указана вторым параметром.Сделать это надо за линейное время без дополнительных аллокаций
+def reverseArray(arr, left, right):
+    while (left < right):
+        arr[left], arr[right] = arr[right], arr[left]
+        left+=1
+        right-=1
+
+
+def solution(arr, k):
+    n = len(arr)
+    reverseArray(arr, 0, n - 1)
+    reverseArray(arr, 0, k%n - 1)
+    reverseArray(arr, k%n, n - 1)
+    return arr
