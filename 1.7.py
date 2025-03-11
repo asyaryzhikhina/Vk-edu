706fe7e61cf93b473fc26a0b4db90ec0402912bb
asyaryzhikhina <142721089+asyaryzhikhina@users.noreply.github.com>
2025-03-11 17:47:31 +0300

    Update 1.7.py

--- 1.7.py
+++ 1.7.py
@@ -1,1 +1,10 @@
-
+#Дан не отсортированный массив целых чисел. Необходимо перенести в начало массива все четные числа, сохраняя их очередность.
+def evenFirst(arr):
+    evenIndex = 0
+    i=0
+    while i<=(len(arr)-1):
+        if arr[i] % 2 == 0:
+            arr[i], arr[evenIndex] = arr[evenIndex], arr[i]
+            evenIndex += 1
+        i+=1
+    return arr
