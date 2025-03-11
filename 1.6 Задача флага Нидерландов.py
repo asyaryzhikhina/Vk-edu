546e0c0c20093b242cdb053a16c8aa7cd640150f
asyaryzhikhina <142721089+asyaryzhikhina@users.noreply.github.com>
2025-03-11 17:20:09 +0300

    Update 1.6 Задача флага Нидерландов.py

--- 1.6 Задача флага Нидерландов.py
+++ 1.6 Задача флага Нидерландов.py
@@ -1,1 +1,16 @@
 #Дан массив состоящий из нулей, единиц и двоек. Необходимо его отсортировать за линейное время.
+def sortColors(nums):
+    low = 0
+    mid = 0
+    high = len(nums) - 1
+    while (mid <= high):
+        if (nums[mid] == 0):
+            nums[low], nums[mid] = nums[mid], nums[low]
+            low+=1
+            mid+=1
+        elif (nums[mid] == 1):
+            mid+=1
+        elif (nums[mid] == 2):
+            nums[mid], nums[high] = nums[high], nums[mid]
+            high-=1
+    return nums
