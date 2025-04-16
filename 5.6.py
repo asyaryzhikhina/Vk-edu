50bcdb2164729e4b43059ba8de486d6ecd0e89e2
asyaryzhikhina <142721089+asyaryzhikhina@users.noreply.github.com>
2025-04-16 12:52:39 +0300

    Update 5.6.py

--- 5.6.py
+++ 5.6.py
@@ -1,1 +1,25 @@
--
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def mirrorTreeIterative(node: TreeNode) -> TreeNode:
+    if node is None:
+        return None
+    
+    queue = deque([node])
+    
+    while queue:
+        current = queue.popleft()  
+
+        current.left, current.right = current.right, current.left
+        
+        if current.left:
+            queue.append(current.left)
+        if current.right:
+            queue.append(current.right)
+    
+    return node
